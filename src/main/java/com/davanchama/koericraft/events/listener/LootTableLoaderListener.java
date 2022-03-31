1623b677dcfb4100433f5f2d1ca27cd842ec7c3d
Davanchama <davanchama@gmail.com>
2022-03-31 13:23:41 +0200

    added that koeri seed one only drops with a probability from grass. added loot tables for every koeri plant in the game.

--- src/main/java/com/davanchama/koericraft/events/listener/LootTableLoaderListener.java
+++ src/main/java/com/davanchama/koericraft/events/listener/LootTableLoaderListener.java
@@ -4,10 +4,9 @@ import com.davanchama.koericraft.init.InitBlocks;
 import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
 import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 import net.minecraft.block.Blocks;
-import net.minecraft.item.Items;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
-import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.util.Identifier;
 
 public class LootTableLoaderListener {
@@ -19,7 +18,7 @@ public class LootTableLoaderListener {
         LootTableLoadingCallback.EVENT.register(((resourceManager, manager, id, supplier, setter) -> {
             if (GRASS_LOOT_TABLE_ID.equals(id)) {
                 FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder().rolls(ConstantLootNumberProvider.create(1)).with(
-                    ItemEntry.builder(InitBlocks.INSTANCE.koeriSeedOne));
+                    ItemEntry.builder(InitBlocks.INSTANCE.koeriSeedOne)).conditionally(RandomChanceLootCondition.builder(0.125f));
                 supplier.pool(poolBuilder);
             }
         }));
